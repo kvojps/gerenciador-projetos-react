a5bb40d86f9d1fac6c55144b9c7cc78c3943d3d4
kvojps <joseferreira.santosjunior@upe.br>
2022-04-14 14:20:02 -0300

    Adicionando os campos preenchidos no formulário de novo projeto ao banco

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -9,6 +9,7 @@ function Input({type, text, name, placeholder, handleOnChange,value}) {
                 name={name} 
                 id={name}
                 placeholder={placeholder}
+                onChange={handleOnChange}
                 value={value}
             />
         </div>
