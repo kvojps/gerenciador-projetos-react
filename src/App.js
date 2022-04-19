518899653723e3ba76141b403797306919c5bd7f
kvojps <joseferreira.santosjunior@upe.br>
2022-04-19 20:03:44 -0300

    adicionando redirecionamento dinamico pelo id para pagina especifica de projeto

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Company from './components/pages/Company'
 import Contact from './components/pages/Contact';
 import NewProject from './components/pages/NewProject'
 import Projects from './components/pages/Projects';
+import Project from './components/pages/Project'
 
 import Container from './components/layout/Container'
 import Navbar from './components/layout/Navbar'
@@ -21,10 +22,11 @@ function App() {
       <Container customClass="min-height">
         <Routes>
           <Route exact path="/" element={<Home/>}></Route>
-          <Route exact path="/company" element={<Company/>}></Route>
-          <Route exact path="/contact" element={<Contact/>}></Route>
-          <Route exact path="/projects" element={<Projects/>}></Route>
-          <Route exact path="/newproject" element={<NewProject/>}></Route>
+          <Route path="/company" element={<Company/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+          <Route path="/projects" element={<Projects/>}></Route>
+          <Route path="/newproject" element={<NewProject/>}></Route>
+          <Route path="/project/:id" element={<Project/>}></Route>
         </Routes>
       </Container>
       
